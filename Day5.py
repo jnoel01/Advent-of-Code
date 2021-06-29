edd4f7c4214e27be59fc22a8c4ce813ecca01519
Jessica Noel <57379770+jnoel01@users.noreply.github.com>
2021-06-29 01:57:13 -0400

    Create Day5.py

---
+++ Day5.py
@@ -0,0 +1,10 @@
+all_seats = [int(
+    (l.replace('B','1').replace('F','0')
+    .replace('R','1').replace('L','0'))
+    ,2) for l in open('input.txt')]
+
+print(f"Part 1: Highest seat is {max(all_seats)}")
+
+your_seat = [s for s in range(min(all_seats), max(all_seats)) 
+            if s not in all_seats][0]
+print(f"Part 2: Your seat is {your_seat}")
