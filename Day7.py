e3293f85b692ebc8492f3a25bb4440b4c54397eb
Jessica Noel <57379770+jnoel01@users.noreply.github.com>
2021-06-29 02:01:51 -0400

    Create Day7.py

---
+++ Day7.py
@@ -0,0 +1,27 @@
+import re
+from collections import defaultdict
+
+bags = defaultdict(dict)
+for l in lines:
+    bag = re.match(r'(.*) bags contain', l).groups()[0]
+    for count, b in re.findall(r'(\d+) (\w+ \w+) bag', l):
+        bags[bag][b] = int(count)
+
+def part1():
+    answer = set()
+    def search(color):
+        for b in bags:
+            if color in bags[b]:
+                answer.add(b)
+                search(b)
+    search('shiny gold')
+    return len(answer)
+
+def part2():
+    def search(bag):
+        count = 1
+        for s in bags[bag]:
+            multiplier = bags[bag][s]
+            count += multiplier * search(s)
+        return count
+    return search('shiny gold' ) - 1
